eaa418a423d98898cdfa8bedcaf84aeff483e823
Bjarke Gotfredsen <gotfredsen@users.noreply.github.com>
2022-12-22 08:50:23 +0200

    Create ILB.h

---
+++ src/ILB.h
@@ -0,0 +1,15 @@
+/*!
+ * @file ILB.h
+ *
+ * Written by @gotfredsen, with contributions from the open source community.
+ *
+ * MIT license.
+ *
+ */
+
+#ifndef _ILB_H_
+#define _ILB_H_
+
+
+
+#endif // _ILB_H_
